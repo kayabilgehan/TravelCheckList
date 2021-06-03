53d843442f443229fd20de23812dd7dba37e2a3a
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-06-03 19:39:39 +0300

    Initial Commit

---
+++ TravelCheckList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelCheckList
+//
+//  Created by Bilgehan KAYA on 3.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
